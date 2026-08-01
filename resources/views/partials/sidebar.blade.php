9b883260b78332d22f8d6564f3f15de32075b191
saadkianidev <saadr@waywegaming.com>
2026-08-01 18:30:53 +0500

    min

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,19 +5,25 @@
         --color-bg: #ffffff;
     }
 
-    .nav-link{
+    .nav-link {
         background-color: var(--color-secondary) !important;
         color: var(--color-bg) !important;
         border-radius: 6px;
     }
-     .nav-link:active{
-         background-color: var(--color-primary) !important;
-     }
+
+    ..nav-link:hover {
+        background-color: var(--color-bg) !important;
+    }
+
+    .nav-link:active {
+        background-color: var(--color-primary) !important;
+    }
 
     .site-sidebar {}
 </style>
 
-<aside class="site-sidebar text-white p-3" style="width: 220px; min-height: 100vh; background-color: var(--color-primary); color: var(--color-bg) !important;">
+<aside class="site-sidebar text-white p-3"
+    style="width: 220px; min-height: 100vh; background-color: var(--color-primary); color: var(--color-bg) !important;">
     <nav class="nav flex-column gap-2">
         <a href="{{ route('posts.index') }}"
             class="nav-link text-white {{ request()->routeIs('posts.index') ? '' : '' }}">

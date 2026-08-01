316f5e4bf2dbcadd0e219c6f899230defddd08ba
saadkianidev <saadr@waywegaming.com>
2026-08-01 18:18:36 +0500

    min

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -8,7 +8,11 @@
     .nav-link{
         background-color: var(--color-secondary) !important;
         color: var(--color-bg) !important;
+        border-radius: 6px;
     }
+     .nav-link:active{
+         background-color: var(--color-primary) !important;
+     }
 
     .site-sidebar {}
 </style>
@@ -16,11 +20,11 @@
 <aside class="site-sidebar text-white p-3" style="width: 220px; min-height: 100vh; background-color: var(--color-primary); color: var(--color-bg) !important;">
     <nav class="nav flex-column gap-2">
         <a href="{{ route('posts.index') }}"
-            class="nav-link text-white {{ request()->routeIs('posts.index') ? 'active bg-secondary rounded' : '' }}">
+            class="nav-link text-white {{ request()->routeIs('posts.index') ? '' : '' }}">
             My Posts
         </a>
         <a href="{{ route('posts.create') }}"
-            class="nav-link text-white {{ request()->routeIs('posts.create') ? 'active bg-secondary rounded' : '' }}">
+            class="nav-link text-white {{ request()->routeIs('posts.create') ? '' : '' }}">
             Create New Post
         </a>
     </nav>
